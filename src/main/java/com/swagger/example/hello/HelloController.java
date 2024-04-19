acee2e644b5f75e815dfc3d7a2054bce34796649
김진한 <kimjh0417@gsitm.com>
2024-04-19 16:33:46 +0900

    ADD swagger FOR meot-blog documentation repository

---
+++ src/main/java/com/swagger/example/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.swagger.example.hello;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity get(){
+        return ResponseEntity.ok("{'data' : 'hello-world' }");
+    }
+}
